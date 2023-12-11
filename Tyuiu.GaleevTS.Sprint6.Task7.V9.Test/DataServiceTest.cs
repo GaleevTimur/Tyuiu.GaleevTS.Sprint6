72eb6c9037aebd636ddb1aae05199431e9cf295d
GaleevTimur <timur.galeev.05@yandex.ru>
2023-12-11 23:06:17 +0500

    Task

--- Tyuiu.GaleevTS.Sprint6.Task7.V9.Test/DataServiceTest.cs
+++ Tyuiu.GaleevTS.Sprint6.Task7.V9.Test/DataServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 
 namespace Tyuiu.GaleevTS.Sprint6.Task7.V9.Test
 {
@@ -9,6 +10,11 @@ namespace Tyuiu.GaleevTS.Sprint6.Task7.V9.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = @"C:\Users\timur_8n182p8\source\repos\Tyuiu.GaleevTS.Sprint6\Tyuiu.GaleevTS.Sprint6.Task7.V9\bin\Debug\OutPutFileTask7V20.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
         }
     }
 }
